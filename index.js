e9307e3e69a63adb8ad61f65ee691ce903583879
Linus Unnebäck <linus@folkdatorn.se>
2018-04-04 15:41:50 +0100

    Drop support for Node.js 0.x and 4.x

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
-var crypto = require('crypto')
-var ignore = require('ignore')
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+const ignore = require('ignore')
 
-var each = require('async-each')
-var md5File = require('md5-file')
+const each = require('async-each')
+const md5File = require('md5-file')
 
 function md5Dir (dirname, options, cb) {
   if (typeof options === 'function') {
@@ -16,7 +16,7 @@ function md5Dir (dirname, options, cb) {
 
   options = options || {}
 
-  var ig = ignore()
+  const ig = ignore()
 
   if (options.ignore) {
     ig.add(options.ignore)
@@ -27,13 +27,13 @@ function md5Dir (dirname, options, cb) {
       if (err) return cb(err)
 
       function iterator (file, cb) {
-        var relativeFilepath = path.join(prefix, file)
+        const relativeFilepath = path.join(prefix, file)
 
         if (ig.ignores(relativeFilepath)) {
           return cb(null, null)
         }
 
-        var absoluteFilepath = path.join(dirname, relativeFilepath)
+        const absoluteFilepath = path.join(dirname, relativeFilepath)
 
         fs.stat(absoluteFilepath, function (err, stat) {
           if (err) return cb(err)
@@ -53,7 +53,7 @@ function md5Dir (dirname, options, cb) {
       each(files, iterator, function done (err, hashes) {
         if (err) return cb(err)
 
-        var hash = crypto.createHash('md5')
+        const hash = crypto.createHash('md5')
 
         hashes.forEach(function (h) {
           if (h !== null) hash.update(h)
@@ -70,25 +70,25 @@ function md5Dir (dirname, options, cb) {
 function md5DirSync (dirname, options) {
   options = options || {}
 
-  var ig = ignore()
+  const ig = ignore()
 
   if (options.ignore) {
     ig.add(options.ignore)
   }
 
   function run (prefix) {
-    var hash = crypto.createHash('md5')
-    var files = fs.readdirSync(path.join(dirname, prefix))
+    const hash = crypto.createHash('md5')
+    const files = fs.readdirSync(path.join(dirname, prefix))
 
-    for (var i = 0; i < files.length; i++) {
-      var relativeFilepath = path.join(prefix, files[i])
+    for (let i = 0; i < files.length; i++) {
+      const relativeFilepath = path.join(prefix, files[i])
 
       if (ig.ignores(relativeFilepath)) {
         continue
       }
 
-      var absoluteFilepath = path.join(dirname, relativeFilepath)
-      var stat = fs.statSync(absoluteFilepath)
+      const absoluteFilepath = path.join(dirname, relativeFilepath)
+      const stat = fs.statSync(absoluteFilepath)
 
       if (stat.isFile()) {
         hash.update(md5File.sync(absoluteFilepath))
